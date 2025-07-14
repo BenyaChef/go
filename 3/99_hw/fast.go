fb9c2a890df4b5f0b4352593bb61fc7933bc4975
Maxim Bineckiy <benyachef@gmail.com>
2025-07-14 18:29:09 +0300

    complete HW 3

--- 3/99_hw/fast.go
+++ 3/99_hw/fast.go
@@ -1,18 +1,242 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/mailru/easyjson"
+	"github.com/mailru/easyjson/jlexer"
+	"github.com/mailru/easyjson/jwriter"
 	"io"
+	"os"
+	"strings"
 )
+
+type User struct {
+	Browsers []string `json:"browsers"`
+	Company  string   `json:"company"`
+	Country  string   `json:"country"`
+	Email    string   `json:"email"`
+	Job      string   `json:"job"`
+	Name     string   `json:"name"`
+	Phone    string   `json:"phone"`
+}
 
 // вам надо написать более быструю оптимальную этой функции
 func FastSearch(out io.Writer) {
-	/*
-		!!! !!! !!!
-		обратите внимание - в задании обязательно нужен отчет
-		делать его лучше в самом начале, когда вы видите уже узкие места, но еще не оптимизировалм их
-		так же обратите внимание на команду в параметром -http
-		перечитайте еще раз задание
-		!!! !!! !!!
-	*/
-	SlowSearch(out)
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(file)
+
+	fileContents, err := io.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	seenBrowsers := make(map[string]struct{})
+	uniqueBrowsers := 0
+	foundUsers := ""
+
+	lines := strings.Split(string(fileContents), "\n")
+
+	for i, line := range lines {
+		user := User{}
+		// fmt.Printf("%v %v\n", err, line)
+		err := easyjson.Unmarshal([]byte(line), &user)
+		if err != nil {
+			panic(err)
+		}
+
+		isAndroid := false
+		isMSIE := false
+
+		browsers := user.Browsers
+		for _, browser := range browsers {
+			if strings.Contains(browser, "Android") {
+				isAndroid = true
+				if _, exist := seenBrowsers[browser]; !exist {
+					seenBrowsers[browser] = struct{}{}
+					uniqueBrowsers++
+				}
+			}
+		}
+
+		for _, browser := range browsers {
+			if strings.Contains(browser, "MSIE") {
+				isMSIE = true
+				if _, exists := seenBrowsers[browser]; !exists {
+					seenBrowsers[browser] = struct{}{}
+					uniqueBrowsers++
+				}
+			}
+		}
+
+		if !(isAndroid && isMSIE) {
+			continue
+		}
+
+		email := strings.ReplaceAll(user.Email, "@", " [at] ")
+		foundUsers += fmt.Sprintf("[%d] %s <%s>\n", i, user.Name, email)
+	}
+	//data, err := json.MarshalIndent(users, "", "  ")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println(string(data))
+	fmt.Fprintln(out, "found users:\n"+foundUsers)
+	fmt.Fprintln(out, "Total unique browsers", uniqueBrowsers)
+}
+
+var (
+	_ *json.RawMessage
+	_ *jlexer.Lexer
+	_ *jwriter.Writer
+	_ easyjson.Marshaler
+)
+
+func easyjson9e1087fdDecodeHw3Data(in *jlexer.Lexer, out *User) {
+	isTopLevel := in.IsStart()
+	if in.IsNull() {
+		if isTopLevel {
+			in.Consumed()
+		}
+		in.Skip()
+		return
+	}
+	in.Delim('{')
+	for !in.IsDelim('}') {
+		key := in.UnsafeFieldName(false)
+		in.WantColon()
+		if in.IsNull() {
+			in.Skip()
+			in.WantComma()
+			continue
+		}
+		switch key {
+		case "browsers":
+			if in.IsNull() {
+				in.Skip()
+				out.Browsers = nil
+			} else {
+				in.Delim('[')
+				if out.Browsers == nil {
+					if !in.IsDelim(']') {
+						out.Browsers = make([]string, 0, 4)
+					} else {
+						out.Browsers = []string{}
+					}
+				} else {
+					out.Browsers = (out.Browsers)[:0]
+				}
+				for !in.IsDelim(']') {
+					var v1 string
+					v1 = string(in.String())
+					out.Browsers = append(out.Browsers, v1)
+					in.WantComma()
+				}
+				in.Delim(']')
+			}
+		case "company":
+			out.Company = string(in.String())
+		case "country":
+			out.Country = string(in.String())
+		case "email":
+			out.Email = string(in.String())
+		case "job":
+			out.Job = string(in.String())
+		case "name":
+			out.Name = string(in.String())
+		case "phone":
+			out.Phone = string(in.String())
+		default:
+			in.SkipRecursive()
+		}
+		in.WantComma()
+	}
+	in.Delim('}')
+	if isTopLevel {
+		in.Consumed()
+	}
+}
+func easyjson9e1087fdEncodeHw3Data(out *jwriter.Writer, in User) {
+	out.RawByte('{')
+	first := true
+	_ = first
+	{
+		const prefix string = ",\"browsers\":"
+		out.RawString(prefix[1:])
+		if in.Browsers == nil && (out.Flags&jwriter.NilSliceAsEmpty) == 0 {
+			out.RawString("null")
+		} else {
+			out.RawByte('[')
+			for v2, v3 := range in.Browsers {
+				if v2 > 0 {
+					out.RawByte(',')
+				}
+				out.String(string(v3))
+			}
+			out.RawByte(']')
+		}
+	}
+	{
+		const prefix string = ",\"company\":"
+		out.RawString(prefix)
+		out.String(string(in.Company))
+	}
+	{
+		const prefix string = ",\"country\":"
+		out.RawString(prefix)
+		out.String(string(in.Country))
+	}
+	{
+		const prefix string = ",\"email\":"
+		out.RawString(prefix)
+		out.String(string(in.Email))
+	}
+	{
+		const prefix string = ",\"job\":"
+		out.RawString(prefix)
+		out.String(string(in.Job))
+	}
+	{
+		const prefix string = ",\"name\":"
+		out.RawString(prefix)
+		out.String(string(in.Name))
+	}
+	{
+		const prefix string = ",\"phone\":"
+		out.RawString(prefix)
+		out.String(string(in.Phone))
+	}
+	out.RawByte('}')
+}
+
+// MarshalJSON supports json.Marshaler interface
+func (v User) MarshalJSON() ([]byte, error) {
+	w := jwriter.Writer{}
+	easyjson9e1087fdEncodeHw3Data(&w, v)
+	return w.Buffer.BuildBytes(), w.Error
+}
+
+// MarshalEasyJSON supports easyjson.Marshaler interface
+func (v User) MarshalEasyJSON(w *jwriter.Writer) {
+	easyjson9e1087fdEncodeHw3Data(w, v)
+}
+
+// UnmarshalJSON supports json.Unmarshaler interface
+func (v *User) UnmarshalJSON(data []byte) error {
+	r := jlexer.Lexer{Data: data}
+	easyjson9e1087fdDecodeHw3Data(&r, v)
+	return r.Error()
+}
+
+// UnmarshalEasyJSON supports easyjson.Unmarshaler interface
+func (v *User) UnmarshalEasyJSON(l *jlexer.Lexer) {
+	easyjson9e1087fdDecodeHw3Data(l, v)
 }
